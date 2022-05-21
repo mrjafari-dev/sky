531cac996dcfdb4a775296a7ed0c6201fbc5f194
mrjafari483 <mrjafari483@Gmail.com>
2022-05-21 01:41:30 -0400

    create Search box in Country List

--- app/src/main/java/dev/mrjafari/weatherapp/MainActivity.kt
+++ app/src/main/java/dev/mrjafari/weatherapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.app.Activity
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
@@ -47,14 +48,10 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dev.mrjafari.weatherapp.contract.MainContract
 import dev.mrjafari.weatherapp.model.CountryModel
 import dev.mrjafari.weatherapp.presenter.MainPresenter
-import dev.mrjafari.weatherapp.ui.theme.Purple200
-import dev.mrjafari.weatherapp.ui.theme.Purple500
-import dev.mrjafari.weatherapp.ui.theme.shadow
-import dev.mrjafari.weatherapp.ui.theme.white
+import dev.mrjafari.weatherapp.ui.theme.*
 import dev.mrjafari.weatherapp.view.MainLayout
 import dev.mrjafari.weatherapp.view.SearchBox
 import kotlinx.coroutines.launch
@@ -64,28 +61,20 @@ class MainActivity : ComponentActivity(), MainContract.View {
     val state = mutableStateOf(true)
     var text = ""
     val Countries = mutableListOf<CountryModel>()
+
     @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val systemUiController = rememberSystemUiController()
-
-            SideEffect {
-                // Update all of the system bar colors to be transparent, and use
-                // dark icons if we're in light theme
-                systemUiController.setSystemBarsColor(
-                    color = white,
-                    darkIcons = true
-                )
-
-                // setStatusBarsColor() and setNavigationBarColor() also exist
+            WeatherAppTheme() {
+                bottemsheet(Countries)
             }
 
-            bottemsheet(Countries)
+
         }
 
-        val presenter = MainPresenter(this,this)
+        val presenter = MainPresenter(this, this)
         presenter.request_data()
         presenter.CountryRequest()
     }
@@ -156,7 +145,10 @@ fun Modifier.coloredShadow(
 
 @ExperimentalMaterialApi
 @Composable
-fun bottemsheet(countries :List<CountryModel>) {
+fun bottemsheet(countries: MutableList<CountryModel>) {
+    var SearchResultCountry = countries
+    var countryList = arrayListOf<CountryModel>()
+
     val coroutineScope = rememberCoroutineScope()
     val bottomSheetScaffoldState =
         rememberBottomSheetScaffoldState(bottomSheetState = BottomSheetState(BottomSheetValue.Collapsed))
@@ -166,22 +158,66 @@ fun bottemsheet(countries :List<CountryModel>) {
         scaffoldState = bottomSheetScaffoldState,
         sheetElevation = 30.dp,
         sheetContent = {
-            val selectedValue = remember { mutableStateOf("") }
-            val label = "Item"
-            Box(Modifier.fillMaxWidth().fillMaxHeight(0.8f).background(white)) {
+            val searchBox = remember {
+                mutableStateOf("")
+            }
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.8f)
+                    .background(white)
+            ) {
                 Column(Modifier.fillMaxSize()) {
-                    Button(onClick = {
-                        ContryName.value = CountrySelected
-                        coroutineScope.launch {
-                            if (bottomSheetScaffoldState.bottomSheetState.isCollapsed)
-                                bottomSheetScaffoldState.bottomSheetState.expand()
-                            else
-                                bottomSheetScaffoldState.bottomSheetState.collapse()
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        TextField(value = searchBox.value, onValueChange = {
+                            searchBox.value = it
+                            if (it.length > 1) {
+
+                                countryList.clear()
+                                countries.forEach { countryModel ->
+                                    if (countryModel.country_name.contains(it)) {
+                                        countryList.add(countryModel)
+                                    }
+                                }
+                                SearchResultCountry = countryList
+                            } else if (it.length < 1) {
+                                SearchResultCountry = countries
+                                countryList.clear()
+                            }
+
+                        }, modifier = Modifier
+                            .fillMaxWidth(0.7f)
+                            .offset(15.dp, 10.dp),
+                            colors = TextFieldDefaults.textFieldColors(
+                                backgroundColor = shadow,
+                                cursorColor = Color.Black,
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent
+                            ),
+                            shape = RoundedCornerShape(10.dp),
+                            label = {
+                                Text(text = "Search Country")
+                            }
+                        )
+
+                        Button(
+                            onClick = {
+                                ContryName.value = CountrySelected
+                                coroutineScope.launch {
+                                    if (bottomSheetScaffoldState.bottomSheetState.isCollapsed)
+                                        bottomSheetScaffoldState.bottomSheetState.expand()
+                                    else
+                                        bottomSheetScaffoldState.bottomSheetState.collapse()
+                                }
+                            }, modifier = Modifier
+                                .fillMaxWidth(0.7f)
+                                .offset(25.dp, 20.dp)
+                        ) {
+                            Text("Down")
                         }
-                    }) {
-                        Text("Down")
                     }
-                    CountrySelected =  Preview_MultipleRadioButtons(countries)
+
+                    CountrySelected = Preview_MultipleRadioButtons(SearchResultCountry)
 
 
                 }
@@ -197,7 +233,7 @@ fun bottemsheet(countries :List<CountryModel>) {
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Preview_MultipleRadioButtons(countries :List<CountryModel>) :String{
+fun Preview_MultipleRadioButtons(countries: MutableList<CountryModel>): String {
 
     val selectedValue = remember { mutableStateOf("") }
 
@@ -206,23 +242,28 @@ fun Preview_MultipleRadioButtons(countries :List<CountryModel>) :String{
 
     val itemss = countries
     Column(Modifier.padding(8.dp)) {
-        Text(text = "Selected value: ${selectedValue.value.ifEmpty { "NONE" }}")
+        /*  Text(
+              text = "Country code: ${selectedValue.value.ifEmpty { "NONE" }}",
+              modifier = Modifier.offset(20.dp)
+          )*/
         LazyVerticalGrid(cells = GridCells.Adaptive(minSize = 130.dp)) {
             items(itemss) {
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.selectable(
-                        selected = isSelectedItem(it.country_code),
-                        onClick = {
-                            onChangeState(it.country_code)
-                        },
-                        role = Role.RadioButton
-                    ).padding(8.dp)
+                    modifier = Modifier
+                        .selectable(
+                            selected = isSelectedItem(it.country_code),
+                            onClick = {
+                                onChangeState(it.country_code)
+                            },
+                            role = Role.RadioButton
+                        )
+                        .padding(8.dp)
                 ) {
-                    val name :String
+                    val name: String
                     if (it.country_name.equals(""))
                         name = it.country_code
-                    else name =it.country_name
+                    else name = it.country_name
                     RadioButton(
                         selected = isSelectedItem(it.country_code),
                         onClick = null
